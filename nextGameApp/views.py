0e08f85493026caf5f617282db30c012436c649a
Andrea <ciorbaandrea04@gmail.com>
2024-11-17 13:28:15 +0200

    homepage

--- nextGameApp/views.py
+++ nextGameApp/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+def home(request):
+    return render(request, 'home.html')
+
+def games(request):
+    return render(request, 'games.html')
+
+def profile(request):
+    return render(request, 'profile.html')
